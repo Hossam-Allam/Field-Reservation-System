7367096958e366c1603afc2f6eb3c3eb9b780bd0
Hossam-Allam <h.refaie@stu.khas.edu.tr>
2025-01-11 20:41:18 +0300

    header displayes name instead of email

--- hf/header.php
+++ hf/header.php
@@ -6,6 +6,7 @@
     <nav>
         <?php
             session_start();
+            require_once 'connect.php';
 
             // Handle logout
             if (isset($_GET['logout'])) {
@@ -17,10 +18,28 @@
 
             $is_logged_in = isset($_SESSION['user_email']);
             $user_email = $is_logged_in ? htmlspecialchars($_SESSION['user_email']) : null;
+            
+            $query = "SELECT name FROM customers WHERE email = ?";
+    
+           // getting name
+            if ($stmt = $conn->prepare($query)) {
+                $stmt->bind_param("s", $user_email);
+                $stmt->execute();
+                $stmt->bind_result($name);
+                
+                
+                if ($stmt->fetch()) {
+                    // HI
+                } else {
+                    $name = "Guest"; 
+                }
+
+                $stmt->close();
+            }
 
             if ($is_logged_in): 
         ?>
-            <span id="white">Welcome, <?php echo $user_email; ?></span>
+            <span id="white">Welcome, <?php echo $name; ?></span>
             <a href="profile.php">Profile</a>
             <a href="?logout=true">Sign Out</a>
         <?php else: ?>
